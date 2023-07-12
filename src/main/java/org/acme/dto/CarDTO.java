f22f348c11f1efd2dff36db2a535e1f659a48c09
gdblsssu <gdblsssu@gmail.com>
2023-07-12 11:24:18 +0300

    Добавил описание OpenApi для DTO авто и человека

--- src/main/java/org/acme/dto/CarDTO.java
+++ src/main/java/org/acme/dto/CarDTO.java
@@ -4,15 +4,20 @@ import jakarta.persistence.Column;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 public class CarDTO {
+    @Schema(title = "Car id", required = true)
     public Long id;
     @NotBlank
+    @Schema(title = "Car name", required = true)
     public String name;
     @NotBlank
+    @Schema(title = "Car color", required = true)
     public String color;
-    @Min(10)
-    @Max(400)
+    @Min(value = 10, message = "The value must be more than 9")
+    @Max(value = 400, message = "The value must be less than 401")
+    @Schema(title = "Car max speed between 10 to 400", required = true)
     public Integer maxSpeed;
 
     public CarDTO(Long id, String name, String color, Integer maxSpeed) {
--- src/main/java/org/acme/dto/PersonDTO.java
+++ src/main/java/org/acme/dto/PersonDTO.java
@@ -5,21 +5,26 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import org.acme.entity.OwnersDocument;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class PersonDTO {
+    @Schema(title = "Person id", required = true)
     Long id;
     @NotBlank
+    @Schema(title = "Person name", required = true)
     public String name;
     @NotBlank
+    @Schema(title = "Person surname", required = true)
     public String surname;
-    
-    @Min(value = 18)
-    @Max(value = 110)
+    @Min(value = 18, message = "The value must be more than 17")
+    @Max(value = 110, message = "The value must be less than 111")
+    @Schema(title = "Person age between 18 to 110", required = true)
     public Integer age;
     @NotBlank
+    @Schema(title = "Person phone", required = true)
     public String phone;
     public Set<OwnersDocument> ownersDocuments = new HashSet<>();
     public PersonDTO(Long id, String name, String surname, Integer age, String phone, Set<OwnersDocument> ownersDocuments) {
