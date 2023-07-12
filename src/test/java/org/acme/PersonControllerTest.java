d05f5fdefcdb7f0f77c815c42baaaaa374e42da0
gdblsssu <gdblsssu@gmail.com>
2023-07-12 18:47:30 +0300

    Добавил тесты для PersonService, PersonController

--- src/test/java/org/acme/PersonControllerTest.java
+++ src/test/java/org/acme/PersonControllerTest.java
@@ -1,6 +1,8 @@
 package org.acme;
 
 import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.acme.service.PersonService;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -8,6 +10,23 @@ import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class PersonControllerTest {
+
+
+    @Test
+    public void testGetPersonsEndpoint() {
+        given()
+                .when().get("/persons")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testGetPersonByIdEndpoint() {
+        given()
+                .when().get("/persons/1")
+                .then()
+                .statusCode(200);
+    }
 
 
 }
---
+++ src/test/java/org/acme/PersonServiceTest.java
@@ -0,0 +1,22 @@
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.acme.dto.PersonDTO;
+import org.acme.service.PersonService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+public class PersonServiceTest {
+    @Inject
+    PersonService personService;
+    @Test
+    public void testGetPersonByIdEndpoint(){
+        PersonDTO personDTO = personService.getById(1L);
+        Assertions.assertEquals("testuser1", personDTO.getName());
+        Assertions.assertEquals("testuser1", personDTO.getSurname());
+        Assertions.assertEquals(21, personDTO.getAge());
+        Assertions.assertEquals("21", personDTO.getPhone());
+    }
+}
