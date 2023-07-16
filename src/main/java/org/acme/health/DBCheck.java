9bc9e005dfa2749c76e68c35d020f054f87c9cf6
gdblsssu <gdblsssu@gmail.com>
2023-07-16 13:17:34 +0300

    Добавил кастомную проверку соединения с БД в health check

--- src/main/java/org/acme/health/DBCheck.java
+++ src/main/java/org/acme/health/DBCheck.java
@@ -1,4 +1,46 @@
 package org.acme.health;
 
-public class DBCheck {
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+import org.jboss.logging.annotations.Properties;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Liveness
+public class DBCheck implements HealthCheck {
+
+    @ConfigProperty(name = "quarkus.datasource.jdbc.url")
+    String url;
+    @ConfigProperty(name = "quarkus.datasource.username")
+    String username;
+    @ConfigProperty(name = "quarkus.datasource.password")
+    String password;
+    @Override
+    public HealthCheckResponse call() {
+
+        if(isConnect()){
+            return HealthCheckResponse.up("DB is connected");
+        } else {
+            return HealthCheckResponse.down("No connection to db");
+        }
+    }
+
+    private boolean isConnect(){
+        try (Connection conn = DriverManager.getConnection(
+                url, username, password)) {
+
+            if (conn != null) {
+                return true;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
 }
