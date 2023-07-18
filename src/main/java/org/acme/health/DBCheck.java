3da364867e69419cebcf82367e09c369f55e499d
gdblsssu <gdblsssu@gmail.com>
2023-07-18 12:24:18 +0300

    Исправил проверку БД в health check

--- src/main/java/org/acme/health/DBCheck.java
+++ src/main/java/org/acme/health/DBCheck.java
@@ -1,11 +1,15 @@
 package org.acme.health;
 
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.Liveness;
 import org.jboss.logging.annotations.Properties;
 
+import java.awt.geom.QuadCurve2D;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -19,28 +23,22 @@ public class DBCheck implements HealthCheck {
     String username;
     @ConfigProperty(name = "quarkus.datasource.password")
     String password;
+
+    @Inject
+    EntityManager entityManager;
+
     @Override
     public HealthCheckResponse call() {
 
-        if(isConnect()){
+        try{
+            Query checkCon = entityManager.createQuery("select 1");
+            checkCon.getResultList();
             return HealthCheckResponse.up("DB is connected");
-        } else {
-            return HealthCheckResponse.down("No connection to db");
+        } catch (Exception e){
+            return HealthCheckResponse.down("Database connection error");
         }
     }
 
-    private boolean isConnect(){
-        try (Connection conn = DriverManager.getConnection(
-                url, username, password)) {
 
-            if (conn != null) {
-                return true;
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
 
 }
